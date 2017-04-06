807c94cf4abcb76dc681bf2fc631e6e2f09b2d34
iguardines <iguardines@gmail.com>
2017-04-06 15:12:06 -0300

    Create tpLogico.pl

---
+++ tpLogico.pl
@@ -0,0 +1,13 @@
+/ * Punto 1: Dominio inicial
+Queremos modelar el negocio de una consultora de sistemas, que en el relevamiento nos contó que:
+Fernando sabe programar COBOL, Visual Basic, y Java.
+Julieta y Marcos programan en Java.
+Aún no sabemos si Julieta programó en Go.
+Santiago programa en ECMAScript y en Java.
+Nadie programó en Assembler.
+Fernando es analista funcional.
+Andrés es project leader pero no programa.
+Alguien es programador, si es que programa en algún lenguaje (no importa si además cumple otro rol).
+Codifique la base de conocimientos en base a lo anteriormente expuesto. En caso de no agregar nada justifique su decisión.
+
+*/
